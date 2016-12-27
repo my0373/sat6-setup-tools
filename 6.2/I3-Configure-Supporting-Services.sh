52a79fc8fe435f07e0469fb2578836bae616d62f
Matt York <my0373@gmail.com>
2016-12-27 18:14:09 +0000

    Ensure that NTP is re-enabled on startup

--- 6.2/I3-Configure-Supporting-Services.sh
+++ 6.2/I3-Configure-Supporting-Services.sh
@@ -9,5 +9,6 @@ cp /etc/ntp.conf /etc/ntp.conf.orig
 cat /etc/ntp.conf | sed 's/^server.*/server 192.168.100.2/' | grep -v "^#" | sort -u > /etc/ntp.conf.new
 cp /etc/ntp.conf.new /etc/ntp.conf
 ntpdate 192.168.100.2
+systemctl enable ntpd
 systemctl start ntpd
 ntpq -p
